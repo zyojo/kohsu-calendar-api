b2da74bcb3a7974f426dbc34a30ff7d8b449a1d4
Nakahara Ryota <zyojozyo@gmail.com>
2022-10-22 16:01:01 +0700

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use("/api/v1/clients", clientRoute);
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL);
+    await connectDB(process.env.MONGO_HEROKU_URL || process.env.MONGO_URL);
     app.listen(process.env.PORT || PORT, console.log("サーバーが起動しました"));
   } catch (err) {
     console.log(err);
